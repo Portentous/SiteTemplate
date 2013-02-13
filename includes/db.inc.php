02526528140210e12e0ec4ca66488d72297e1cff
Chris Bonnell <cjbonn2005@hotmail.com>
2013-02-13 15:28:31 -0500

    first commit

---
+++ includes/db.inc.php
@@ -0,0 +1,30 @@
+<?
+if($_SERVER['REMOTE_ADDR'] == '127.0.0.1'){
+	$host = "localhost";
+	$db = "pitclub";
+	$user = "root";
+	$pass = "";
+}else{
+	$host = ""; // Host IP
+	$db = "";   // Database Name
+	$user = ""; // Database User
+	$pass = ""; // Database Password
+}
+try {
+	$pdo = new PDO("mysql:host={$host};dbname={$db}",$user,$pass);
+} catch (PDOException $e){
+	die("There was a problem connecting to the database.");
+}
+// Website Constants and configurations
+if(session_id() == '') session_start();
+
+$config = array();
+$configurations = $pdo->query("SELECT name,value FROM config");
+while(list($name,$value) = $configurations->fetch()){
+	$config[$name] = $value;
+}
+define("SITE_EMAIL",$config['siteemail']);
+define("SITE_NAME",$config['sitename']);
+define("SITE_URL",$config['siteurl']);
+define("SCRIPT_PATH",$config['scriptpath']);
+?>
---
+++ js/main.js
@@ -0,0 +1,1 @@
+// JavaScripts, if needed.
